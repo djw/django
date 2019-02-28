1bd645d151eb79a5a5862b7ece4f57b002a7656f
Dan Wilson <danjwilson@gmail.com>
2019-02-28 15:10:33 +0000

    Added UglifyJS-based JavaScript compression
    Implemented via Gruntfile, configured to match current behaviour
    as closely as possible.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,28 @@ module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
             all: ['js_tests/tests.html']
+        },
+        uglify: {
+            admin: {
+                options: {
+                    output: {
+                        max_line_len: 500
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'django/contrib/admin/static/admin/js/',
+                    dest: 'django/contrib/admin/static/admin/js/',
+                    src: ['actions.js', 'collapse.js', 'inlines.js', 'prepopulate.js'],
+                    rename: (dest, src) => {
+                        return dest + '/' + src.replace('.js', '.min.js');
+                    }
+                }]
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.registerTask('test', ['qunit']);
     grunt.registerTask('default', ['test']);
